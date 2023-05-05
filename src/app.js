7068239adf0b3a85ed6f003860eb883208c6c284
JohanEkstroem <90635692+JohanEkstroem@users.noreply.github.com>
2023-05-05 23:02:04 +0200

    Add getAllById endpoint

--- src/app.js
+++ src/app.js
@@ -3,7 +3,12 @@ import fruitBoxes from './util/data.js'
 const app = express();
 const PORT = 5080;
 
-app.get("/", (req, res) => res.send(getAll()))
+app.get("/fruitboxes", (req, res) => res.send(getAll()))
+
+app.get('/fruitboxes/:number', (req, res) => {
+  let getAllById = fruitBoxes.filter(x => x.Id == req.params.number)
+  res.send(getAllById);
+});
 
 app.listen(PORT, console.log(`Listening to port ${PORT}`))
 
