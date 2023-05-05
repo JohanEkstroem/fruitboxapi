91f87f9cf203f132437ee349ffe0759355563c3b
JohanEkstroem <90635692+JohanEkstroem@users.noreply.github.com>
2023-05-05 22:25:36 +0200

    Add express

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import express  from "express";
+const app = express();
+const PORT = 5080;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!")
+})
+
+app.listen(PORT, console.log(`Listening to port ${PORT}`))
