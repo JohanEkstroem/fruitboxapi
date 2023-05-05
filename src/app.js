e939321500365054c66cc9ba9c9163ec572298c7
JohanEkstroem <90635692+JohanEkstroem@users.noreply.github.com>
2023-05-05 22:49:22 +0200

    Add getAll endpoint

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 import express from "express";
+import fruitBoxes from './util/data.js'
 const app = express();
 const PORT = 5080;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!")
-})
+app.get("/", (req, res) => res.send(getAll()))
 
 app.listen(PORT, console.log(`Listening to port ${PORT}`))
+
+const getAll = () => fruitBoxes
